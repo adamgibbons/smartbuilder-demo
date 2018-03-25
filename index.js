d1f119b2ed9078c924d998b2823044c43e1b1e5b
Adam Gibbons <adam.d.gibbons@gmail.com>
2018-03-25 12:13:05 -0600

    simplify markup

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 (function() {
-  $('[data-block]').find('input[type=radio]').change(function(e) {
-    var $container = $(e.target).closest('[data-block]')
+  $('form').find('input[type=radio]').change(function(e) {
+    var $container = $(e.target).closest('form')
 
     $($container).find('.messaging').children().hide()
     $($container).find('button').fadeIn(250)
   })
 
-  $('[data-block]').find('button').click(function(e) {
-    var $container = $(e.target).closest('[data-block]')
+  $('form').submit(function(e) {
+    e.preventDefault();
+
+    var $container = $(e.target)
     var result = $container.find('input:checked');
 
     $container.find('.messaging [data-response=' + $(result).attr('id') + ']').show();
